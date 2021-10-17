4bb1b1a197e8fb557486375a51903e8a7772af53
Stanislav <stanislav@iMac-Stanislav.local>
2021-10-17 20:25:01 +0300

    Created model for hard coded answer and small fix

--- 8ball-magic/Controller/ResponseViewController.swift
+++ 8ball-magic/Controller/ResponseViewController.swift
@@ -11,43 +11,7 @@ class ResponseViewController: UIViewController {
     
     private var responseModel = ResponseModel()
     private var responseView = ResponseView()
-    
-    private let answerLabel: UILabel = {
-        let label = UILabel()
-        let maxSize = CGSize(width: 150, height: 300)
-        let size = label.sizeThatFits(maxSize)
-        label.text = Text.shakeMe
-        label.textColor = .black
-        label.textAlignment = .center
-        label.numberOfLines = 3
-        label.font = UIFont.boldSystemFont(ofSize: 17)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.frame = CGRect(origin: CGPoint(x: 100, y: 100), size: size)
-        return label
-    }()
-    
-    private let magicLabel: UILabel = {
-        let label = UILabel()
-        label.text = Text.iAmAMagicBall
-        label.textColor = .black
-        label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 24)
-        label.numberOfLines = 2
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let askLabel: UILabel = {
-        let label = UILabel()
-        label.text = Text.askMeAnything
-        label.textColor = .black
-        label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.numberOfLines = 2
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
+
     override func loadView() {
         view = responseView
     }
@@ -55,29 +19,8 @@ class ResponseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        //        view.addSubview(magicLabel)
-        //        view.addSubview(askLabel)
-        //        view.addSubview(answerLabel)
-        //  setupLayout()
     }
-    
-    
-    private func setupLayout() {
-        magicLabel.topAnchor.constraint(
-            equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        magicLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
-        magicLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
-        
-        askLabel.topAnchor.constraint(equalTo: magicLabel.bottomAnchor, constant: 10).isActive = true
-        askLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
-        askLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
-        
-        answerLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
-        answerLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-        answerLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
-        answerLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
-    }
-    
+
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
             self.responseModel.getAnswer(completion: { answer in
---
+++ 8ball-magic/Model/HardCodedAnswerModel.swift
@@ -0,0 +1,12 @@
+//
+//  HadrCodedAnswerModel.swift
+//  8ball-magic
+//
+//  Created by Stanislav on 17.10.2021.
+//
+
+import Foundation
+
+struct HardCodedAnswersModel {
+    var motivationAnswers = ["JUST DO IT!!!", "YES!!!", "CHANGE YOUR MIND!!!", "KEEP MOVING!!!"]
+}
